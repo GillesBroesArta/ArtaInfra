14d04a69c617d1cd28e259fc9e4d0c4b72871884
Robby Demoustiez <robby.demoustiez@artilium.com>
2018-02-01 17:06:40 +0100

    Add extension method for IsValidJson

--- Extensions.cs
+++ Extensions.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
+using Newtonsoft.Json;
 using Remotion.Linq.Parsing.Structure;
 
 namespace Arta.Infrastructure
@@ -13,5 +14,22 @@ namespace Arta.Infrastructure
     public static class Extensions
     {
         public static bool IsSuccessStatusCode(this HttpStatusCode statusCode) => ((int)statusCode >= 200) && ((int)statusCode <= 299);
+
+        public static bool IsValidJson(this string json)
+        {
+            try
+            {
+                JsonConvert.DeserializeObject(json);
+            }
+            catch (JsonReaderException)
+            {
+                //Catch only specific exception thrown on invalid json
+                //bubble other deserialize exceptions to result in a http 500 status
+                return false;
+            }
+
+            return true;
+        }
+
     }
 }
