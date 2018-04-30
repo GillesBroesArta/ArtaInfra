612acffea8072ea52f37ba92d2acfdbf8639b7bb
Jelle Bleyaert <jelle.bleyaert@artilium.com>
2018-04-30 17:33:44 +0200

    Fixing lower-uppercase problem

---
+++ Utils/Helpers/AuthorizationHelper.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Arta.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Primitives;
+
+namespace ArtaInfra.Utils.Helpers
+{
+    public static class AuthorizationHelper
+    {   
+        private static bool hasClaimWithValue(HttpContextAccessor httpContextAccessor, string type, string value) {
+            return httpContextAccessor.HttpContext.User.Claims.Any(c => c.Type == type && c.Value == value);
+        }
+        public static bool isCspAdmin(HttpContextAccessor httpContextAccessor, string csp)
+        {
+            return hasClaimWithValue(httpContextAccessor, "isCspAdmin", csp);
+        }
+        public static bool isCspSales(HttpContextAccessor httpContextAccessor, string csp)
+        {
+            return hasClaimWithValue(httpContextAccessor, "isCspSales", csp);
+        }
+        public static bool isCspPlatformConfiguration(HttpContextAccessor httpContextAccessor, string csp)
+        {
+            return hasClaimWithValue(httpContextAccessor, "isCspPlatformConfiguration", csp);
+        }
+        public static bool isCspPartnerSupport(HttpContextAccessor httpContextAccessor, string csp)
+        {
+            return hasClaimWithValue(httpContextAccessor, "isCspPartnerSupport", csp);
+        }
+    }        
+}
