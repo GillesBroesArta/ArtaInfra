5488ff2019e0634b525606424568427fd77f1e3f
Peter Favere <peter.favere@artilium.com>
2018-06-25 11:28:08 +0200

    Add generic sorting to IQueryable, add missing reference

--- Utils/Extentions/QueryableExtensions.cs
+++ Utils/Extentions/QueryableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Internal;
